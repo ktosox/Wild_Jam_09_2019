419e4fed385177589f2b1fa0a0e265fce6d883e6
Ktosox <ktoso3@gmail.com>
2019-09-16 19:34:46 +0200

    added tank assetes, selector now checks if cells are valid

--- Game_Parts/Board.gd
+++ Game_Parts/Board.gd
@@ -4,6 +4,7 @@ const tileOffset = Vector2(22,13)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var invalidCells = [-1,9,15]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,12 +15,21 @@ func _input(event):
 	if(event is InputEventMouseMotion):
 		track_mouse()
 	if(event.is_action_pressed("LMB")):
-		set_cellv(world_to_map(get_global_mouse_position()),0)
+		set_cellv(world_to_map(get_global_mouse_position()),-1)
 
 func track_mouse():
 	var cellPosition = world_to_map(get_global_mouse_position())
 	$selector.global_position = global_to_grid(get_global_mouse_position())
+	if(is_cell_valid(cellPosition)):
+		$selector.modulate = Color(1,1,1)
+	else:
+		$selector.modulate = Color(0,0,1)
 
+func is_cell_valid(cell = Vector2()):
+	if(invalidCells.has(get_cellv(cell))):
+		return false
+	else:
+		return true
 
 func cell_to_grid(cell = Vector2()):
 	var newPosition = Vector2()
